b280726189dee02eae1fa9fdd3d7c1d2e2c055c0
John Asto <john.azt.dev@gmail.com>
2023-03-16 15:59:37 -0300

    add readme

--- src/server/models/task-list.js
+++ src/server/models/task-list.js
@@ -1,55 +1,35 @@
-const Task = require('./task');
+const Task = require("./task");
 
 class TasksList {
-	constructor() {
-		this.tasks = [new Task('Learn Angular'), new Task('Practice React')];
-	}
+  constructor() {
+    this.tasks = [new Task("Learn Angular"), new Task("Practice React")];
+  }
 
-	createTask(name) {
-		const newTask = new Task(name);
-		this.tasks.push(newTask);
-		return this.tasks;
-	}
+  createTask(name) {
+    const newTask = new Task(name);
+    this.tasks.push(newTask);
+    return this.tasks;
+  }
 
-	deleteTask(id) {
-		this.tasks = this.tasks.filter(task => task.id !== id);
-	}
+  deleteTask(id) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 
-	completeTask(id) {
-		const task = this.tasks.find(t => t.id === id);
-		if (task) {
-			return (task.completed = !task.completed);
-		}
-	}
-	updateTask(id, newName) {
-		const task = this.tasks.find(t => t.id === id);
-		if (task) {
-			task.name = newName;
-		}
-	}
-	getTasks() {
-		return this.tasks;
-	}
-
-	//   increaseVotes(id) {
-	//     this.bands = this.bands.map((band) => {
-	//       if (band.id === id) {
-	//         band.votes += 1;
-	//       }
-
-	//       return band;
-	//     });
-	//   }
-
-	//   changeName(id, newName) {
-	//     this.bands = this.bands.map((band) => {
-	//       if (band.id === id) {
-	//         band.name = newName;
-	//       }
-
-	//       return band;
-	//     });
-	//   }
+  completeTask(id) {
+    const task = this.tasks.find((t) => t.id === id);
+    if (task) {
+      return (task.completed = !task.completed);
+    }
+  }
+  updateTask(id, newName) {
+    const task = this.tasks.find((t) => t.id === id);
+    if (task) {
+      task.name = newName;
+    }
+  }
+  getTasks() {
+    return this.tasks;
+  }
 }
 
 module.exports = TasksList;
